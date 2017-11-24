51a84e225392e7e8bac0437f5cf62a507df08444
Daisuke Hirata <daisukihirata@gmail.com>
2017-11-24 10:58:14 +0900

    docker login test

--- hello.go
+++ hello.go
@@ -7,7 +7,7 @@ func main() {
 }
 
 func sayHello() {
-    fmt.Println("Hello world!!!")
+    fmt.Println("Hello world!!! rebuild docker")
 //    return
 //    fmt.Println("Unreachable line") // got vet error test
 }
