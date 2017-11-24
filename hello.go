49a1695d0f3b6ea59650a5017b3e2f0d2527b262
Daisuke Hirata <daisukihirata@gmail.com>
2017-11-25 00:42:51 +0900

    fumi test 7 - reviewdog api token added

--- hello.go
+++ hello.go
@@ -7,7 +7,7 @@ func main() {
 }
 
 func sayHello() {
-    fmt.Println("Hello world!!! reviewdog command updated")
+    fmt.Println("Hello world!!! reviewdog api token added")
 //    return
 //    fmt.Println("Unreachable line") // got vet error test
 }
