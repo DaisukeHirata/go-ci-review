3c6b1ad27ef3322a6f93690f24f5b70999f24181
Daisuke Hirata <daisukihirata@gmail.com>
2017-12-03 12:16:46 +0900

    fumi test 18 - test golint reviewdog

--- hello.go
+++ hello.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    sayHello()
+    SayHello()
 }
 
 func SayHello() {
  fmt.Println("Hello world!!! delete blanks in the beginning of this line!!") // gofmt test
-//    return
-//    fmt.Println("Unreachable line") // got vet error test
+    return
+    fmt.Println("Unreachable line") // got vet error test
 }
