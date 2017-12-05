5395696ea1503a0182ac3a2d617881999bfe00b3
Daisuke Hirata <daisukihirata@gmail.com>
2017-12-05 17:32:07 +0900

    fumi test 19 - install unused command and show help

--- hello.go
+++ hello.go
@@ -8,6 +8,6 @@ func main() {
 
 func SayHello() {
  fmt.Println("Hello world!!! delete blanks in the beginning of this line!!") // gofmt test
-    return
-    fmt.Println("Unreachable line") // got vet error test
+//    return
+//    fmt.Println("Unreachable line") // got vet error test
 }
