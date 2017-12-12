cddb50db99262d28afef62a74a512b3e214d907e
Daisuke Hirata <daisukihirata@gmail.com>
2017-12-12 14:47:08 +0900

    fumi test 22 - cart & test are added.

--- cart.go
+++ cart.go
@@ -1,20 +1,32 @@
-package cart
+package main
+
+//import "errors"
+
+//func examle(code string) (int,err) {
+//    if code == "hoge" {
+//        return 1,nil
+//    }
+//    return 0,errors.New("code must be hoge")
+//}
+
+//func main() {
+//	New()
+//}//
 
 type Cart struct {
-       products []string
+	products []string
 }
 
 func New() *Cart {
-       c := new(Cart)
-       c.products = make ([]string, 0)
-       return c
+	c := new(Cart)
+	c.products = make([]string, 0)
+	return c
 }
 
 func (c *Cart) Add(s string) {
-       c.products = append(c.products, s)
+	c.products = append(c.products, s)
 }
 
 func (c *Cart) GetAll() []string {
-       return c.products
+	return c.products
 }
-
